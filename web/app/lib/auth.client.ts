0cb7240e5c51006e0605e6954562e917fcf182bb
Anand Dubey <87166787+Anand24816@users.noreply.github.com>
2024-11-02 18:48:26 +1100

    read var from ENV

--- web/app/lib/auth.client.ts
+++ web/app/lib/auth.client.ts
@@ -3,7 +3,7 @@ import { createAuthClient } from "better-auth/react"
 // https://remix.run/docs/en/main/guides/envvars#browser-environment-variables
 
 const authClient = createAuthClient({
-  baseURL: 'http://localhost:5173' // the base url of your auth server, TODO: How to pass for production?
+  baseURL: window.ENV.BETTER_AUTH_URL
 })
 
 export const { signIn, signUp, signOut, useSession } = authClient
